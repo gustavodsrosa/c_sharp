2de4ef50408f45c2b2686c0414b5bd51f1db3b44
gustavodsrosa <g.dsantosrosa@gmail.com>
2020-12-02 21:22:03 -0300

    Alterações

--- Atividade_02122020/Classes/Aluno.cs
+++ Atividade_02122020/Classes/Aluno.cs
@@ -1,27 +1,27 @@
 using System;
 
-namespace Atividade_02122020.Classes {
+    namespace Atividade_02122020.Classes {
 
-    public class Aluno {
-        public string nome;
-        public string curso;
-        public int idade;
-        public string rg;
-        public bool bolsista;
-        public double mediaFinal;
-        public double valorDaMensalidade;
-        public void VerMediaFinal(double media) {
-            Console.WriteLine($"Média do aluno: {media}");
-        }
-        public void VerMensalidade(bool temBolsa, double mensalidade) {
-            if(temBolsa == true) {
-                Console.WriteLine("Esse aluno é bolsista!");
-                Console.WriteLine($"Mensalidade sem desconto: {mensalidade}");
-                Console.WriteLine($"Mensalidade com desconto: {0.7 * mensalidade}");
-            } else {
-                Console.WriteLine("Esse aluno NÃO é bolsista!");
-                Console.WriteLine($"Mensalidade: {mensalidade} ");
+        public class Aluno {
+            public string nome;
+            public string curso;
+            public int idade;
+            public string rg;
+            public bool bolsista;
+            public double mediaFinal;
+            public double valorDaMensalidade;
+            public void VerMediaFinal(double media) {
+                Console.WriteLine($"Média do aluno: {media}");
             }
-    }
-}
+            public void VerMensalidade(bool temBolsa, double mensalidade) {
+                if(temBolsa == true) {
+                    Console.WriteLine("Esse aluno é bolsista!");
+                    Console.WriteLine($"Mensalidade sem desconto: {mensalidade}");
+                    Console.WriteLine($"Mensalidade com desconto: {0.7 * mensalidade}");
+                } else {
+                    Console.WriteLine("Esse aluno NÃO é bolsista!");
+                    Console.WriteLine($"Mensalidade: {mensalidade} ");
+                }
+            }
+        }
 }
---
+++ AulaPOO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AulaPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celular celular = new Celular();
+
+            Console.WriteLine("Está ligado? s/n");
+            string escolha = Console.ReadLine();
+
+            if(escolha == "s") {
+                celular.ligado = true;
+            } else if(escolha == "n") {
+                celular.ligado = false;
+            }
+
+            do {
+                Console.WriteLine("Digite a cor: ");
+                celular.cor = Console.ReadLine();
+
+                Console.WriteLine("Digite o modelo: ");
+                celular.modelo = Console.ReadLine();
+
+                Console.WriteLine("Digite o tamanho: ");
+                celular.tamanho = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite o telefone: ");
+                celular.telefone = Console.ReadLine();
+
+                celular.FazerLigacao(celular.telefone);
+                celular.EnviarMensagem(celular.telefone);
+            } while(celular.ligado == false);
+            
+
+
+        }
+    }
+}
