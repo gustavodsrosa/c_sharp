ada2d5f27cb42701f4906a16311cda10b52d7d4c
gustavodsrosa <g.dsantosrosa@gmail.com>
2020-12-17 15:59:33 -0300

    Alterações

---
+++ POO_ObjetoArgumento/Classes/Carrinho.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System;
+
+namespace POO_ObjetoArgumento.Classes
+{
+    public class Carrinho
+    {
+        public float ValorTotal { get; set; }
+        List<Produto> carrinho = new List<Produto>();
+        public void AdicionarProduto(Produto _produto) {
+            carrinho.Add(_produto);
+        }
+        public void MostrarProduto() {
+            if(carrinho != null) {
+                foreach(var item in carrinho) {
+                    Console.WriteLine($"{item.Codigo}, {item.Nome}, {item.Preco}");
+                }
+            } else {
+                Console.WriteLine("Não há nenhum produto no carrinho!");
+            }
+        }
+        public void RemoverProduto(Produto _produto) {
+            carrinho.Remove(_produto);
+        }
+        public void MostrarValorTotal() {
+            ValorTotal = 0;
+            if(carrinho != null) {
+                foreach (var item in carrinho) {
+                    ValorTotal += item.Preco;
+                }
+                Console.WriteLine($"O total do carrinho é: {ValorTotal}");
+            } else {
+                Console.WriteLine("O carrinho está vazio, tente novamente!");
+            }
+        }
+        public void AlterarItem(int _codigo_procurado, Produto _novoProduto) {
+            carrinho.Find(produto => produto.Codigo == _codigo_procurado).Nome = _novoProduto.Nome;
+            carrinho.Find(produto => produto.Codigo == _codigo_procurado).Preco = _novoProduto.Preco;
+        }
+        
+        
+    }
+}
---
+++ POO_ObjetoArgumento/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using POO_ObjetoArgumento.Classes;
+
+namespace POO_ObjetoArgumento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto(23, "Mousepad", 29.99f);
+            Produto p2 = new Produto(52, "Teclado", 529.99f);
+            Produto p3 = new Produto(78, "Headset", 899.99f);
+            Produto p4 = new Produto(23, "Mouse", 329.99f);
+
+            Carrinho cart = new Carrinho();
+            cart.AdicionarProduto(p1);
+            cart.AdicionarProduto(p2);
+            cart.AdicionarProduto(p3);
+            cart.AdicionarProduto(p4);
+
+            cart.MostrarProduto();
+        }
+    }
+}
